6d1807194c5a9e24f090096649d8bb7e6d2664fd
dinhphu <2335Cube#>
2026-01-21 18:13:10 +0700

    bài 9: login shop api

--- src/services/keytoken.service.js
+++ src/services/keytoken.service.js
@@ -1,15 +1,32 @@
 "use strict";
 
-const keyTokenModel = require("../models/keytoken.model");
+const keytokenModel = require("../models/keytoken.model");
 
 class KeyTokenService {
-  static createKeyToken = async ({ userId, publicKey, privateKey }) => {
+  static createKeyToken = async ({
+    userId,
+    publicKey,
+    privateKey,
+    refreshToken,
+  }) => {
     try {
-      const tokens = await keyTokenModel.create({
-        user: userId,
-        publicKey,
-        privateKey,
-      });
+      // level 0
+      // const tokens = await keyTokenModel.create({
+      //   user: userId,
+      //   publicKey,
+      //   privateKey,
+      // });
+      // return tokens ? tokens.publicKey : null;
+
+      // level 1
+      const filters = { user: userId },
+        update = { publicKey, privateKey, refreshTokensUsed: [], refreshToken },
+        options = { upsert: true, new: true };
+      const tokens = await keytokenModel.findOneAndUpdate(
+        filters,
+        update,
+        options,
+      );
       return tokens ? tokens.publicKey : null;
     } catch (error) {
       console.error("Error creating key token:", error);
---
+++ src/services/shop.service.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const shopModel = require("../models/shop.model");
+
+const findByEmail = async ({
+  email,
+  select = {
+    email: 1,
+    password: 1,
+    name: 1,
+    status: 1,
+    roles: 1,
+  },
+}) => {
+  // logic to find shop by email from database
+  return await shopModel.findOne({ email }).select(select).lean(); // Giả sử shopModel đã được định nghĩa và kết nối với cơ sở dữ liệu
+};
+
+module.exports = { findByEmail };
