503c9787f2669ba938861cfcd7b919b7242c536e
dinhphu <2335Cube#>
2026-01-21 15:38:53 +0700

    bài 8: API sử dụng class để phản hồi(respon)

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -2,17 +2,20 @@
 
 const accessService = require("../services/access.service");
 
+// AcceptedResponse = OK, CreatedResponse = created
+const {  AcceptedResponse,  CreatedResponse,} = require("../core/success.response");
+
 class AccessController {
   signUp = async (req, res, next) => {
-    return res
-      .status(201)
-      .json(
-        await accessService.signUp(
-          req.body.name,
-          req.body.email,
-          req.body.password,
-        ),
-      );
+    new CreatedResponse({
+      message: "SHOP: User created successfully",
+      metadata: await accessService.signUp(
+        req.body.name,
+        req.body.email,
+        req.body.password,
+      ),
+      options: { limit: 10 },
+    }).send(res);
   };
 }
 
--- src/core/success.response.js
+++ src/core/success.response.js
@@ -7,24 +7,47 @@ const STATUS_CODE = {
   NO_CONTENT: 204,
 };
 
-class CuccessResponse {
+const REASON_STATUS_CODE = {
+  SUCCESS: "SUCCESS: The request has succeeded.",
+  CREATED: "CREATED: The request has been fulfilled and resulted in a new resource being created.",
+  ACCEPTED: "ACCEPTED: The request has been accepted for processing, but the processing has not been completed.",
+  NO_CONTENT: "NO CONTENT: The server successfully processed the request, but is not returning any content.",
+};
+
+class SuccessResponse {
   constructor({
-    message = "Success",
-    status = "success",
-    code = 200,
-    metadata = null,
+    message,
+    statusCode = STATUS_CODE.SUCCESS,
+    reasonStatusCode = REASON_STATUS_CODE.SUCCESS,
+    metadata = {},
   }) {
-    this.message = message;
-    this.status = status;
-    this.code = code;
+    this.message = !message ? reasonStatusCode : message;
+    this.status = statusCode;
     this.metadata = metadata;
   }
 
   send(res, headers = {}) {
-    return res.status(this.code).json({
-      message: this.message,
-      status: this.status,
-      metadata: this.metadata,
-    });
+    return res.status(this.status).json(this);
   }
 }
+
+class AcceptedResponse extends SuccessResponse {
+  constructor({ message, metadata }) {
+    super({ message, metadata });
+  }
+}
+
+class CreatedResponse extends SuccessResponse {
+  constructor({
+    options,
+    message,
+    statusCode = STATUS_CODE.CREATED,
+    reasonStatusCode = REASON_STATUS_CODE.CREATED,
+    metadata,
+  }) {
+    super({ message, statusCode, reasonStatusCode, metadata });
+    this.options = options;
+  }
+}
+
+module.exports = { SuccessResponse, AcceptedResponse, CreatedResponse };
